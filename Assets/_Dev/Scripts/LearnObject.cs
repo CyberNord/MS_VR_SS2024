324a2d0bf75c83d7570c17d52c22c45a636a4aea
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2024-03-18 11:32:05 +0100

    18.03.2024 [11:30] #1 basic Objects & Manager

---
+++ Assets/_Dev/Scripts/LearnObject.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace _Dev.Scripts
+{
+    public class LearnObject
+    {
+        private readonly GameObject _asset;
+        private readonly int _id; 
+        private readonly AudioClip _audioClipEnglish; 
+        private readonly AudioClip _audioClipGerman; 
+        private readonly AudioClip _audioClipVimmi;
+        private readonly string _descGerman;
+        private readonly string _descEnglish;
+        private readonly string _descVimmi;
+        
+
+        public GameObject Asset => _asset;
+        public int Id => _id;
+        public AudioClip AudioClipEnglish => _audioClipEnglish;
+        public AudioClip AudioClipGerman => _audioClipGerman;
+        public AudioClip AudioClipVimmi => _audioClipVimmi;
+        public string DescGerman => _descGerman;
+        public string DescEnglish => _descEnglish;
+        public string DescVimmi => _descVimmi;
+
+        public LearnObject(GameObject asset, AudioClip audioClipEnglish, AudioClip audioClipGerman,
+            AudioClip audioClipVimmi, string descGerman, string descEnglish, string descVimmi)
+        {
+            _asset = asset;
+            _audioClipEnglish = audioClipEnglish;
+            _audioClipGerman = audioClipGerman;
+            _audioClipVimmi = audioClipVimmi;
+            _descGerman = descGerman;
+            _descEnglish = descEnglish;
+            _descVimmi = descVimmi;
+        }
+    }
+}
---
+++ Assets/_Dev/Scripts/LearnObjectManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace _Dev.Scripts
+{
+    public class Database
+    {
+        private readonly List<LearnObject> _learnObjects;
+
+        public Database()
+        {
+            _learnObjects = new List<LearnObject>();
+        }
+
+        public void AddLearnObject(LearnObject learnObject)
+        {
+            _learnObjects.Add(learnObject);
+        }
+        
+        
+        public List<LearnObject>[] GetLearnObjectGroups()
+        {
+            if (_learnObjects.Count < 30)
+            {
+                return null;
+            }
+            // Sort by german to ensure consistent Sorting
+            var sortedLearnObjects = _learnObjects.OrderBy(lo => lo.DescGerman).ToList();
+
+            List<LearnObject>[] groups = new List<LearnObject>[3];
+            for (int i = 0; i < groups.Length; i++)
+            {
+                // Skipping ensures each group gets a unique set of LearnObjects
+                groups[i] = sortedLearnObjects.Skip(i * 10).Take(10).ToList();
+            }
+
+            return groups;
+        }
+    }
+}
